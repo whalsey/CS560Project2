48c496b6b7b05525135d2e5600f10d6c931b1774
IsaacSherman <sherman1337@gmail.com>
2016-03-24 11:24:40 -0400

    Testing input file parsing

--- preprocessor.py
+++ preprocessor.py
@@ -2,4 +2,16 @@
 
 fileName = 'pg100.txt'
 
-lines = open(fileName).readlines()[150:]
+lines = open(fileName).readlines()[175:]
+
+newLines = []
+index = 175
+for line in lines:
+    newLine = line.strip() + str(index) + '\n'
+    index += 1
+    newLines.append(newLine)
+
+newFile = open('pg100Editted.txt', 'w')
+for line in newLines:
+    newFile.write(line)
+
