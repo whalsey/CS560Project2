319114607a25cc4fff5fd81855be58cf53a43a78
IsaacSherman <sherman1337@gmail.com>
2016-03-24 12:14:55 -0400

    Input file parsing mostly done

--- preprocessor.py
+++ preprocessor.py
@@ -4,14 +4,37 @@ fileName = 'pg100.txt'
 
 lines = open(fileName).readlines()[175:]
 
+wordCounts = {}
 newLines = []
-index = 175
+index = 176
 for line in lines:
-    newLine = line.strip() + str(index) + '\n'
+    if len(line.strip()) !=0:
+        newLine = line.strip(".,:#$%^&*()[]?!\n").lower() + ' ' + str(index) + '\n'
+        found = newLine.find("\' ")
+        if found != -1:
+            newLine = newLine[1:found] + newLine[found+1:-1]+'\n'
+        found = newLine.find(" \'")
+        if found != -1:
+            newLine = newLine[1:found+1] + newLine[found+2:-1]+'\n'
+        words = newLine.split(' ')
+        for word in words:
+            if word in wordCounts:
+                wordCounts[word] +=1
+            else:
+                wordCounts[word] =1
+        newLines.append(newLine)
     index += 1
-    newLines.append(newLine)
 
 newFile = open('pg100Editted.txt', 'w')
 for line in newLines:
     newFile.write(line)
 
+wordCountFile = open('wc.txt', 'w')
+
+import operator
+
+sorted_x = sorted(wordCounts.items(), key=operator.itemgetter(1),reverse=True)
+
+
+for pairs in sorted_x:
+    wordCountFile.write(pairs[0].strip() + ":" + str(pairs[1]) + '\n')
