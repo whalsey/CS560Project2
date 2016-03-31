3db9007483aaf4e1c52375326b69a0d1fad541b3
IsaacSherman <sherman1337@gmail.com>
2016-03-31 10:55:11 -0400

    Rewriting Mapper.py for directory reading

--- Mapper.py
+++ Mapper.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 import sys
+import os
 
 class Mapper:
     '''Takes a number of lines (with the line number appended after a space) and returns a list of words and indices'''
     stopWords = []
+    listOfFiles = []
     fout = sys.stdout
     fin = sys.stdin
-    def __init__(self, foutname: str = None, finname: str = None) -> None:
+    def __init__(self, foutname: str = None, dirname: str = None) -> None:
         if foutname is not None:
             self.fout = open(foutname, 'w')
-        if finname is not None:
-            self.fin = open(finname, 'r')
+        self.readDirectory(dirname)
         self.readStopWords()
+
     def readStopWords(self):
         if len(self.stopWords) == 0:
             file = "stopwords.txt"
@@ -19,21 +21,51 @@ class Mapper:
             for line in lines:
                 self.stopWords.append(line.strip().strip())
 
-    def map(self, line):
+    def processLine(self, line):
         line = line.strip()
         words = line.split(' ')
-        index = int(words[-1])
+        try:
+            index = int(words[-1])
+        except ValueError:
+            print("Please ensure all files in the directory are properly formatted (run preprocessing.py on each file).")
+            exit()
+
         words.pop()#Pop the appended line number
         for word in words:
             if word not in self.stopWords:
                 self.fout.write(word + "\t" + str(index)+'\n')
 
+
+    def map(self, fileName):
+        file = []
+        try:
+            file = open(fileName, 'r').readlines()
+        except IOError:
+            print("Directory")
+            return
+
+        for line in file:
+            self.fout.write(fileName + ":")
+            self.processLine(line)
+
+    def readDirectory(self, dirname):
+        self.listOfFiles = os.listdir(dirname)
+
+
     @staticmethod
-    def main(foutname="mappedOut.txt", finname="pg100Editted.txt"):
-        m = Mapper()
-        m.__init__(foutname, finname)
-        for line in m.fin:
-            m.map(line)
+    def main(foutname="mappedOut.txt", dirname="pg100Editted.txt"):
+        m = Mapper(foutname, dirname)
+        for file in m.listOfFiles:
+            m.map(file)
+
+
 
 if __name__ == "__main__":
-    Mapper.main(None, None)
+    arg1 = None
+    arg2 = "./"
+    if len(sys.argv) > 2:
+        arg1 = sys.argv[2]
+        arg2 = sys.argv[1]
+    elif len(sys.argv) == 2:
+        arg2 = sys.argv[1]
+    Mapper.main(arg1, arg2)
